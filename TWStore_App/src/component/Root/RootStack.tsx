e169227cd921421b16559da6a289f7ce5aa90e72
hhhoang02 <135586647+hhhoang02@users.noreply.github.com>
2024-02-21 22:52:23 +0700

    Delete TWStore_App directory

--- TWStore_App/src/component/Root/RootStack.tsx
+++
@@ -1,9 +0,0 @@
-import { getHeaderTitle } from "@react-navigation/elements";
-
-
-export const configStack: any = (props: any) => {
-    return {
-        headerShown: false,
-        animation: 'slide_from_right',
-    }
-}
